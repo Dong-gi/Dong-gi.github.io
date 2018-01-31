be43e87e10725813e924ca750e7cd41efa68845e
Dong-gi <hi.donggi@gmail.com>
2018-01-31 23:50:32 +0900

    Design Changed

---
+++ source/default.js
@@ -0,0 +1,203 @@
+var index = {};
+var indexInv = {};
+var lv1dropup;
+var lv2dropup;
+var lv3dropup;
+
+$(function () {
+    $("nav#top-nav").load("/source/nav-top.html", (response, status, xhr) => {
+        if (status == "success") {
+            $("footer#main-footer").load("/source/footer.html", (response, status, xhr) => {
+                if (status == "success") {
+                    $("nav#bottom-nav").load("/source/nav-bottom.html", (response, status, xhr) => {
+                        if (status == "success") {
+                            /* 모든 콘텐츠에 id 부여 */
+                            var contentID = 0;
+                            var contents = $(".con-lv-1, .con-lv-2, .con-lv-3");
+                            for (var i = 0; i < contents.length; ++i) {
+                                var id = 'con-id-' + contentID++;
+                                index[id] = [];
+                                $(contents[i]).attr('id', id);
+                            }
+
+                            /* 계층을 이룬 id들에 대해 색인 생성 */
+                            var conLv1s = $(".con-lv-1");
+                            for (var i = 0; i < conLv1s.length; ++i) {
+                                var conLv2s = $(conLv1s[i]).find(".con-lv-2");
+                                for (var j = 0; j < conLv2s.length; ++j) {
+                                    index[$(conLv1s[i]).attr('id')].push($(conLv2s[j]).attr('id'));
+                                    var conLv3s = $(conLv2s[j]).find(".con-lv-3");
+                                    for (var k = 0; k < conLv3s.length; ++k) {
+                                        index[$(conLv2s[j]).attr('id')].push($(conLv3s[k]).attr('id'));
+                                        indexInv[$(conLv3s[k]).attr('id')] = [$(conLv1s[i]).attr('id'), $(conLv2s[j]).attr('id')];
+                                    }
+                                }
+                            }
+
+                            /* 색인 초기 갱신 */
+                            lv1dropup = $("#dropup-lv-1 .dropdown-menu");
+                            lv2dropup = $("#dropup-lv-2 .dropdown-menu");
+                            lv3dropup = $("#dropup-lv-3 .dropdown-menu");
+                            for (var i = 0; i < conLv1s.length; ++i) {
+                                lv1dropup.append($('<a class="dropdown-item" href="javascript:updateDropupManually(\'' + $(conLv1s[i]).attr('id') + '\');">' + $(conLv1s[i]).attr('con-title') + '</a>'));
+                            }
+                            setWidthLimit();
+
+                            /* 색인 자동 갱신 */
+                            var conLv3s = $(".con-lv-3");
+                            window.onscroll = function () {
+                                var pos = (document.scrollTop || window.pageYOffset) - (document.clientTop || 0) + 52;
+                                var current = conLv3s[0];
+                                for (var i = 0; i < conLv3s.length; ++i) {
+                                    if (pos < $(conLv3s[i]).offset().top) {
+                                        break;
+                                    }
+                                    current = conLv3s[i];
+                                }
+
+                                var conLv1 = indexInv[$(current).attr('id')][0];
+                                $("#dropup-lv-1 button").text($("#" + conLv1).attr('con-title'));
+                                var subIndex = index[conLv1];
+                                lv2dropup.empty();
+                                for (var i = 0; i < subIndex.length; ++i) {
+                                    lv2dropup.append('<a class="dropdown-item" href="javascript:updateDropupManually(\'' + subIndex[i] + '\');">' + $('#' + subIndex[i]).attr('con-title') + '</a>');
+                                }
+
+                                var conLv2 = indexInv[$(current).attr('id')][1];
+                                $("#dropup-lv-2 button").text($("#" + conLv2).attr('con-title'));
+                                subIndex = index[conLv2];
+                                lv3dropup.empty();
+                                for (var i = 0; i < subIndex.length; ++i) {
+                                    lv3dropup.append('<a class="dropdown-item" href="javascript:updateDropupManually(\'' + subIndex[i] + '\');">' + $('#' + subIndex[i]).attr('con-title') + '</a>');
+                                }
+
+                                $("#dropup-lv-3 button").text($(current).attr('con-title'));
+                            };
+
+                            $(window).resize(function () { setWidthLimit(); });
+                        }
+                    });
+                }
+            });
+        }
+    });
+});
+function updateDropupManually(id) {
+    var tag = $("#" + id);
+    var cls = tag.attr('class');
+    var lv = cls.indexOf('lv-1') >= 0 ? 1 : (cls.indexOf('lv-2') >= 0 ? 2 : 3);
+    switch (lv) {
+        case 1:
+            $("#dropup-lv-1 button").text(tag.attr('con-title'));
+            $("#dropup-lv-2 button").text('개요 2');
+            $("#dropup-lv-3 button").text('개요 3');
+            lv2dropup.empty();
+            lv3dropup.empty();
+            var subIndex = index[id];
+            for (var i = 0; i < subIndex.length; ++i) {
+                lv2dropup.append('<a class="dropdown-item" href="javascript:updateDropupManually(\'' + subIndex[i] + '\');">' + $('#' + subIndex[i]).attr('con-title') + '</a>');
+            }
+            break;
+        case 2:
+            $("#dropup-lv-2 button").text(tag.attr('con-title'));
+            $("#dropup-lv-3 button").text('개요 3');
+            lv3dropup.empty();
+            var subIndex = index[id];
+            for (var i = 0; i < subIndex.length; ++i) {
+                lv3dropup.append('<a class="dropdown-item" href="javascript:updateDropupManually(\'' + subIndex[i] + '\');">' + $('#' + subIndex[i]).attr('con-title') + '</a>');
+            }
+            break;
+        case 3:
+            $("#dropup-lv-3 button").text(tag.attr('con-title'));
+            break;
+    }
+    $('html, body').animate({
+        scrollTop: tag.offset().top - 52
+    }, 500);
+}
+function setWidthLimit() {
+    try {
+        var tags = $('nav.fixed-bottom button');
+        var newWidth = window.innerWidth / 5;
+        for (var i = 0; i < tags.length; ++i) {
+            $(tags[i]).css('max-width', newWidth);
+            $(tags[i]).css('overflow', 'auto');
+        }
+        tags = $('nav.fixed-bottom div.dropdown-menu a');
+        newWidth = newWidth > 150 ? newWidth : 150;
+        for (var i = 0; i < tags.length; ++i) {
+            $(tags[i]).css('max-width', newWidth);
+            $(tags[i]).css('overflow', 'auto');
+        }
+    } catch (err) { console.log(err); }
+}
+
+var codeSet = new Set();
+function showCode(fileName, lan) {
+    var original = fileName;
+    fileName = fileName.replace(/\W|\s/g, '');
+    if (!codeSet.has(original)) {
+        $("body").append(
+            '<div id="code-' + fileName + '" class="modal code-modal" tabindex="-1" role="dialog">\
+                <div class="modal-dialog modal-dialog-centered modal-lg" role="document">\
+                    <div class="modal-content">\
+                        <div class="modal-header">\
+                            <h2 style="display: inline-block;" class="modal-title">' + original + '</h2>\
+                            <button style="float: right;" type="button" class="close" data-dismiss="modal" aria-label="Close">\
+                                <span aria-hidden="true" style="color: black; font-size: 2em; font-weight: bold;">&times;</span>\
+                            </button>\
+                        </div>\
+                        <div class="modal-body">\
+                            <pre><code class="' + lan + '">\
+                                <p>Modal body text goes here.</p>\
+                            </code></pre>\
+                        </div>\
+                        <div class="modal-footer">\
+                            <button type="button" class="btn btn-primary copy">Copy</button>\
+                            <button type="button" class="btn btn-primary download">Download</button>\
+                            <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>\
+                        </div>\
+                    </div>\
+                </div>\
+            </div>');
+        codeSet.add(original);
+        $("div#code-" + fileName + " div.modal-body code").load((original.indexOf('/') < 0 ? "./" : "") + original, (response, status, xhr) => {
+            $("div#code-" + fileName + " div.modal-body code").html(hljs.highlight(lan, $("div#code-" + fileName + " div.modal-body code").text())['value'].trim());
+            $("div#code-" + fileName).modal("show");
+        });
+        $("div#code-" + fileName + " button.copy").click(() => {
+            copyToClipboard($("div#code-" + fileName + " div.modal-body"));
+            $("div#code-" + fileName).focus();
+        });
+        $("div#code-" + fileName + " button.download").click(() => {
+            downloadCode(original.split('/').pop(), $("div#code-" + fileName + " div.modal-body").text());
+        });
+    } else {
+        $("div#code-" + fileName).modal("show");
+    }
+}
+function copyToClipboard(element) {
+    var hiddenElement = $('<textarea></textarea>');
+    element.append(hiddenElement);
+    hiddenElement.text($(element).text().trim());
+    hiddenElement.select();
+    document.execCommand("copy");
+    showSnackbar("Copied!!", element);
+    hiddenElement.remove();
+}
+function downloadCode(title, text) {
+    var hiddenElement = document.createElement('a');
+    hiddenElement.href = 'data:attachment/text,' + encodeURI(text.trim());
+    hiddenElement.target = '_blank';
+    hiddenElement.download = title;
+    hiddenElement.click();
+}
+function showSnackbar(text, parent, timeout) {
+    var hiddenElement = $('<div id="snackbar">' + text + '</div>');
+    $(parent || 'body').append(hiddenElement);
+    hiddenElement.addClass('show');
+    setTimeout(function () {
+        hiddenElement.removeClass('show');
+        hiddenElement.remove();
+    }, timeout || 1000);
+}
---
+++ study/java/basic.java
@@ -0,0 +1,45 @@
+﻿public class Main {
+
+    public static void main(String[] args) {
+        boolean var01; // true, false
+        char var02; // 2바이트, '\u0000' ~ '\uFFFF'
+        byte var03; // 1바이트, -128 ~ 127
+        short var04; // 2바이트, -2^15 ~ 2^15-1
+        int var05; // 4바이트, -2^31 ~ 2^31-1
+        long var06; // 8바이트, -2^63 ~ 2^63-1
+        float var07; // 4바이트, IEEE 754
+        double var08; // 8바이트, IEEE 754
+        
+        var05 = 123 + 0x123 + 0123 + 0b101101; // 10진수, 16진수, 8진수, 2진수
+        var06 = 123L; // long 리터럴
+        var07 = 1.23F; // float 리터럴
+        var08 = 123_456_000 + 1.234_567 + (1.234E+2);
+
+        // 위 8가지 기본 자료형을 제외한 모든 자료형은 참조 자료형으로, 동적할당된다.
+        int[] arr1;
+        int arr2[];
+        arr1 = new int[10];
+        arr2 = new int[] { 1, 2, 3 };
+        int[][] arr3 = new int[2][];
+        arr3[0] = new int[10];
+        arr3[1] = new int[20];
+
+        String str1 = 123 + "";
+        // 3/0 : ArithmeticException
+        // 3/0. : INF
+        // 0./0. : NaN(Not a Number)
+
+        var05 <<= 1; // 빈 비트를 0으로 채운다.
+        var05 >>= 1; // 부호 비트를 유지한다.
+        var05 >>>= 1; // 비어진 비트를 0으로 채운다.
+
+        // Boolean Logical Operators
+        var01 = false & true;
+        var01 = true | false;
+        var01 = true ^ false; // xor
+        var01 = !true; // not
+        var01 = false && true; // Short Circuit
+        var01 = true || false; // Short Circuit
+    }
+
+}
