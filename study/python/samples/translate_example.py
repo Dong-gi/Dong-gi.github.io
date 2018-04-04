74b702629f7009786821612036453c74663ae3cb
Dong-gi <hi.donggi@gmail.com>
2018-04-04 23:51:52 +0900

    update

--- study/python/samples/translate_example.py
+++ study/python/samples/translate_example.py
@@ -1,6 +1,10 @@
-words1 = ('apple', 'egg')
-words2 = ('사과', '달걀')
+reverse_map = {(ord('a')+c): chr(ord('z')-c) for c in range(0, 26)}
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+trans = str.maketrans(alphabet, alphabet[::-1])
 
-trans = str.maketrans(words1, words2)
-s = '사과 1개 500원, 달걀 1판 3000원'
+print(reverse_map)
+print(trans)
+
+s = 'apple PIE'
+print(s.translate(reverse_map))
 print(s.translate(trans))
