813d569a1b45e2e6db3bf2b8b0cb9c89eaffd230
Dong-gi <hi.donggi@gmail.com>
2018-04-12 23:08:15 +0900

    update

--- study/java/Java10/src/Updates.java
+++ study/java/Java10/src/Updates.java
@@ -1,14 +1,25 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class Updates {
 
 	public static void main(String[] args) throws Exception {
-		var url = new URL("http://www.oracle.com/"); 
-		var conn = url.openConnection(); 
-		var reader = new BufferedReader(
-		    new InputStreamReader(conn.getInputStream()));
+		var arr = new int[] {1, 2, 3, 4};
+		for(var n : arr) {
+			for(var i = 0; i < n; ++i) {
+				System.out.print(i + " ");
+			}
+		}
+
+		try(var out = new ByteArrayOutputStream() {
+			@Override
+			public void close() throws IOException {
+				super.close();
+				System.out.println("Closed!!");
+			}
+		}) {
+			out.write("Hello".getBytes());
+		}
 	}
 
 }
