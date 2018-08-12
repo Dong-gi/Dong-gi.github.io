dd37425fbe816f2024c522d5093bc806407a3ae7
Donggi Kim <hi.donggi@gmail.com>
2018-08-12 23:52:24 +0900

    add

---
+++ JavaScript/Variable.js
@@ -0,0 +1,13 @@
+let num = 5; // 변수 선언 + 초기화
+let var1; // undefined로 초기화됨
+let _, $; // 단독으로도 이용 가능
+
+const PI = 3.141592; // 상수 선언
+
+const AUTO_CLOSE = Symbol("close()가 자동으로 호출될 것입니다");
+
+let obj = {
+    attr : 'value'
+};
+obj.attr2 = 'value2';
+obj['attr3'] = 'value3';
