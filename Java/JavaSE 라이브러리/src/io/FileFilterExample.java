690b89cb9a5c6fc26fdda3c0b3707fe2cae26a6d
Donggi Kim <hi.donggi@gmail.com>
2018-08-12 13:38:12 +0900

    add

---
+++ Java/JavaSE 라이브러리/src/io/FileFilterExample.java
@@ -0,0 +1,21 @@
+package io;
+
+import java.io.File;
+
+public class FileFilterExample {
+
+    public static void main(String[] args) {
+        var root = new File("C:\\");
+        
+        System.out.println("전체 목록");
+        for(var file : root.listFiles()) {
+            System.out.print(file.getName() + ", ");
+        }
+        
+        System.out.println("\n\n공백이 있는 목록");
+        for(var file : root.listFiles((file) -> file.getName().contains(" "))) {
+            System.out.print(file.getName() + ", ");
+        }
+    }
+
+}
---
+++ Java/JavaSE 라이브러리/src/io/SerializableExample.java
@@ -0,0 +1,42 @@
+package io;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class SerializableExample {
+
+    public static class Student implements Serializable {
+        private static final long serialVersionUID = 1L; // == default
+
+        private String name;
+        private int id;
+
+        public Student() {}
+
+        public Student(String name, int id) {
+            this.name = name;
+            this.id = id;
+        }
+
+        @Override
+        public String toString() {
+            return "name: " + name + ", id: " + id;
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        var student = new Student("김동기", 2014136011);
+
+        try(var out = new ObjectOutputStream(new FileOutputStream("student.data"));
+                var in = new ObjectInputStream(new FileInputStream("student.data"));) {
+            out.writeObject(student);
+            student = (Student) in.readObject();
+            System.out.println(student.toString());
+        }
+    }
+
+}
