9ab2c4ab5ca4d159e98278ed66eed35ba0a68b99
Donggi Kim <hi.donggi@gmail.com>
2018-08-12 15:37:13 +0900

    add

---
+++ Java/JavaSE 라이브러리/src/io/ConsoleExample.java
@@ -0,0 +1,19 @@
+package io;
+
+import java.util.Optional;
+
+public class ConsoleExample {
+
+    public static void main(String[] args) {
+        var console = Optional.ofNullable(System.console());
+        console.ifPresentOrElse(c -> System.out.println(c), () -> System.out.println("Run on cmd"));
+
+        char[] password;
+        while (console.isPresent() &&
+                (password = console.get().readPassword("Password: ")) != null) {
+            console.get().printf("Confirm: %s", new String(password)).format("\n").flush();
+            java.util.Arrays.fill(password, ' ');
+        }
+    }
+
+}
