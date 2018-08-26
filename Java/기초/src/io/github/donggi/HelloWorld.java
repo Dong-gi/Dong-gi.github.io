10404f0e727c84eb958ae8f613ffc856a9da6904
Donggi Kim <hi.donggi@gmail.com>
2018-08-26 21:47:55 +0900

    HelloWorld 추가

--- Java/기초/src/io/github/donggi/HelloWorld.java
+++ Java/기초/src/io/github/donggi/HelloWorld.java
@@ -2,9 +2,8 @@ package io.github.donggi;
 
 public class HelloWorld {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World");
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 
 }
