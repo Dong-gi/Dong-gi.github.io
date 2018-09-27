471206ad03cee98f6881d7cb2c785c812ad979d2
김동기 <hi.donggi@gmail.com>
2018-09-27 23:24:10 +0900

    업데이트

---
+++ Java/Basic/src/io/github/donggi/Lambda2.java
@@ -0,0 +1,34 @@
+package io.github.donggi;
+
+public class Lambda2 {
+
+    @FunctionalInterface
+    interface Joiner{
+        String join(String s1, String s2);
+    }
+
+    @FunctionalInterface
+    interface Adder{
+        int add(int a, int b);
+    }
+    
+    public static void test(Joiner joiner){
+        String s1 = "Hello";
+        String s2 = "World";
+        System.out.println(joiner.join(s1, s2));
+    }
+    
+    public static void test(Adder adder){
+        int n1 = 10;
+        int n2 = 5;
+        System.out.println(adder.add(n1, n2));
+    }
+    
+    public static void main(String[] args) {
+        test((String x, String y) -> x+ "," + y);
+        test((Joiner)((x, y) -> x+ " " + y));
+        
+        Adder adder = (x,y)->x+y;
+        test(adder);
+    }
+}
--- Java/Basic/src/io/github/donggi/StreamExample.java
+++ Java/Basic/src/io/github/donggi/StreamExample.java
@@ -1,11 +1,25 @@
 package io.github.donggi;
 
+import java.util.Arrays;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamExample {
 
     public static void main(String[] args) {
-        new Random().ints(10).forEach(i -> System.out.println(i));
+        System.out.println(
+                new Random()
+                    .ints(10)
+                    .mapToObj(String::valueOf)
+                    .collect(Collectors.joining(", "))
+                );
+        
+        System.out.println(
+                Arrays.toString(
+                        Stream.iterate(0, i -> i+2).limit(20).toArray()
+                    )
+                );
     }
 
 }
