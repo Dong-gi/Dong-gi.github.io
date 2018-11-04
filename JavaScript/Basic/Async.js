4034b22e742a3aac9732451c20eeb30431d88649
김동기 <hi.donggi@gmail.com>
2018-11-04 23:23:37 +0900

    update

---
+++ JavaScript/Basic/Async.js
@@ -0,0 +1,19 @@
+
+function sleep(ms) {
+    return new Promise(_ => setTimeout(_, ms));
+}
+
+function hello1() {
+    console.log(1);
+    sleep(1000);
+    console.log(2);
+}
+
+async function hello2() {
+    console.log(10);
+    await sleep(1000);
+    console.log(20);
+}
+
+hello1();
+hello2();
