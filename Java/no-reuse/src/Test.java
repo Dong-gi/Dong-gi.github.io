bb7a3f053433dbc6730d90b4d226adb78783a9c7
dev_donggi <hi.donggi@gmail.com>
2019-06-20 22:50:54 +0900

    update

--- Java/no-reuse/src/Test.java
+++ Java/no-reuse/src/Test.java
@@ -1,45 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
 public class Test {
-    
-    private int num1;
-    private int num2;
-    
-    public Test() { System.out.println(this); }
-    public Test(int num1) { this.num1 = num1; }
-    
-    public Test(int num1, int num2) {
-        // this(num1).num2 = num2; // Syntax error on token ".", ; expected at Test.<init>
-        this(num1);
-        this.num2 = num2;
-    }
-    
-    @Override
-    public String toString() {
-        return num1 + "," + num2;
-    }
-    
+
+    @SuppressWarnings("serial")
     public static void main(String[] args) {
-        System.out.println(new Test(1, 2));
-        act1(Test::new);
-        act2(Test::new);
+        test(new ArrayList<Integer>() {{add(1);}});
+        test(new ArrayList<Byte>() {{add((byte)1);}});
+        test(new ArrayList<String>() {{add("Hello");}});
     }
-    
-    @FunctionalInterface
-    interface Act1 {
-        Test act();
-    }
-    public static void act1(Act1 act1) {
-        System.out.println(act1.act());
-    }
-    
-    @FunctionalInterface
-    interface Act2 {
-        void act();
-    }
-    public static void act2(Act2 act2) {
-        act2.act();
+
+    public static <T> void test(List<T> l) {
+        System.out.println(l.get(0).getClass().getName());
     }
 }
