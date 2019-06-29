f5ba496e7db30ed679ff6b96951b653e7ab746a5
dev_donggi <hi.donggi@gmail.com>
2019-06-29 17:32:10 +0900

    .

--- Java/Basic/src/io/github/donggi/HangulUtility.java
+++ Java/Basic/src/io/github/donggi/HangulUtility.java
@@ -6,30 +6,33 @@ public class HangulUtility {
         System.out.println(
                 new HangulStringBuilder()
                     .append("‘-이/-가’와 ‘-을/-를’").unNun()
-                    .append(" 앞말").iGa()
-                    .append(" 다른 말에 대하여 갖는 일정한 자격").ulRul()
-                    .append(" 나타내는 조사인 ‘격조사’이고, ‘-은/-는’").unNun()
-                    .append(" 어떤 특별한 뜻").ulRul()
-                    .append(" 더해 주는 조사인 보조사라는 점").ulRul()
-                    .append(" 고려할 필요").iGa()
-                    .append(" 있습니다.\n주격 조사 ‘-이/-가’").unNun()
-                    .append(" ‘산").iGa()
-                    .append(" 높다/바다").iGa()
-                    .append(" 깊다’와 같이, 그 앞말").ulRul()
-                    .append(" 문장에서 주어").iGa()
-                    .append(" 되게 하는 일").ulRul()
-                    .append(" 하고, 목적격 조사 ‘-을/-를’").unNun()
-                    .append(" ‘책").ulRul()
-                    .append(" 보다/사과").ulRul()
-                    .append(" 먹다’와 같이, 그 앞말").ulRul()
-                    .append(" 문장에서 목적어").iGa()
-                    .append(" 되게 하는 일").ulRul()
-                    .append(" 하며,\n보조사 ‘-은/-는’").unNun()
-                    .append(" ‘인생").unNun()
-                    .append(" 짧고, 예술").unNun()
-                    .append(" 길다.’와 같이 어떤 대상").iGa()
-                    .append(" 다른 것과 대조됨").ulRul()
-                    .append(" 나타냅니다.")
+                    .append("앞말").iGa()
+                    .append("다른 말에 대하여 갖는 일정한 자격").ulRul()
+                    .append("나타내는 조사인 ‘격조사’이고, ‘-은/-는’").unNun()
+                    .append("어떤 특별한 뜻").ulRul()
+                    .append("더해 주는 조사인 보조사라는 점").ulRul()
+                    .append("고려할 필요").iGa()
+                    .append("있습니다.\n주격 조사 ‘-이/-가’").unNun()
+                    .append("‘산").iGa()
+                    .append("높다/바다").iGa()
+                    .append("깊다’").waGwa()
+                    .append("같이, 그 앞말").ulRul()
+                    .append("문장에서 주어").iGa()
+                    .append("되게 하는 일").ulRul()
+                    .append("하고, 목적격 조사 ‘-을/-를’").unNun()
+                    .append("‘책").ulRul()
+                    .append("보다/사과").ulRul()
+                    .append("먹다’와 같이, 그 앞말").ulRul()
+                    .append("문장에서 목적어").iGa()
+                    .append("되게 하는 일").ulRul()
+                    .append("하며,\n보조사 ‘-은/-는’").unNun()
+                    .append("‘인생").unNun()
+                    .append("짧고, 예술").unNun()
+                    .append("길다.’").waGwa()
+                    .append("같이 어떤 대상").iGa()
+                    .append("다른 것").waGwa()
+                    .append("대조됨").ulRul()
+                    .append("나타냅니다.")
                 );
         /* ‘-이/-가’와 ‘-을/-를’은 앞말이 다른 말에 대하여 갖는 일정한 자격을 나타내는 조사인 ‘격조사’이고, ‘-은/-는’은 어떤 특별한 뜻을 더해 주는 조사인 보조사라는 점을 고려할 필요가 있습니다.
          * 주격 조사 ‘-이/-가’는 ‘산이 높다/바다가 깊다’와 같이, 그 앞말을 문장에서 주어가 되게 하는 일을 하고, 목적격 조사 ‘-을/-를’은 ‘책을 보다/사과를 먹다’와 같이, 그 앞말을 문장에서 목적어가 되게 하는 일을 하며,
@@ -70,6 +73,7 @@ public class HangulUtility {
             for(var idx = builder.length() - 1; idx >= 0; --idx)
                 try {
                     builder.append(hasJongseong(builder.charAt(idx))? '은' : '는');
+                    builder.append(' ');
                     break;
                 } catch(Exception e) {}
             return this;
@@ -79,6 +83,7 @@ public class HangulUtility {
             for(var idx = builder.length() - 1; idx >= 0; --idx)
                 try {
                     builder.append(hasJongseong(builder.charAt(idx))? '이' : '가');
+                    builder.append(' ');
                     break;
                 } catch(Exception e) {}
             return this;
@@ -88,6 +93,29 @@ public class HangulUtility {
             for(var idx = builder.length() - 1; idx >= 0; --idx)
                 try {
                     builder.append(hasJongseong(builder.charAt(idx))? '을' : '를');
+                    builder.append(' ');
+                    break;
+                } catch(Exception e) {}
+            return this;
+        }
+
+        public HangulStringBuilder waGwa() {
+            for(var idx = builder.length() - 1; idx >= 0; --idx)
+                try {
+                    builder.append(hasJongseong(builder.charAt(idx))? '과' : '와');
+                    builder.append(' ');
+                    break;
+                } catch(Exception e) {}
+            return this;
+        }
+
+        public HangulStringBuilder roUro() {
+            for(var idx = builder.length() - 1; idx >= 0; --idx)
+                try {
+                	if(hasJongseong(builder.charAt(idx)))
+                		builder.append('으');
+                    builder.append('로');
+                    builder.append(' ');
                     break;
                 } catch(Exception e) {}
             return this;
