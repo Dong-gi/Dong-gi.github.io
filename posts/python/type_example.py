e470d1aaa69dd4527019b9e023ae07d840d68870
dev_donggi <hi.donggi@gmail.com>
2019-07-22 22:44:43 +0900

    .

--- posts/python/type_example.py
+++ posts/python/type_example.py
@@ -51,3 +51,108 @@ print('Hello World!'.rjust(30))
 
 print('Hello World!'.replace('l', '?')) # He??o Wor?d! # 정규표현식 이용 가능
 print('Hello World!'.replace('l', '?', 2)) # He??o World! # 바꿀 횟수
+
+
+print([1, 2, 3, 4, 5]) # [1, 2, 3, 4, 5]
+print(list('Hello World!')) # ['H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!']
+
+list1 = list('Hello World!')
+print(list1[3]) # l
+print(list1[::-1]) # ['!', 'd', 'l', 'r', 'o', 'W', ' ', 'o', 'l', 'l', 'e', 'H']
+
+list1 = list('Hello ')
+list1 += ('!@#$%')
+list1.extend(('abcde'))
+print(list1) # ['H', 'e', 'l', 'l', 'o', ' ', '!', '@', '#', '$', '%', 'a', 'b', 'c', 'd', 'e']
+
+list1 = list('abcabcabc')
+list1.append('=')
+print(list1) # ['a', 'b', 'c', 'a', 'b', 'c', 'a', 'b', 'c', '=']
+list1.insert(3, '3')
+print(list1) # ['a', 'b', 'c', '3', 'a', 'b', 'c', 'a', 'b', 'c', '=']
+list1.remove('b')
+print(list1) # ['a', 'c', '3', 'a', 'b', 'c', 'a', 'b', 'c', '=']
+del list1[0]
+print(list1) # ['c', '3', 'a', 'b', 'c', 'a', 'b', 'c', '=']
+del list1[-1]
+print(list1) # ['c', '3', 'a', 'b', 'c', 'a', 'b', 'c']
+list1.pop()
+print(list1) # ['c', '3', 'a', 'b', 'c', 'a', 'b']
+
+list1 = list('abcabcabc')
+print(list1.index('b')) # 1 # index() : 위치 확인
+print('c' in list1) # True # in : 포함 여부 확인
+print(list1.count('a') ) # 3 # count() : 요소 개수 확인
+
+list1 = list('abcabcabc')
+list1.sort()
+print(list1) # ['a', 'a', 'a', 'b', 'b', 'b', 'c', 'c', 'c']
+list1.sort(reverse=True)
+print(list1) # ['c', 'c', 'c', 'b', 'b', 'b', 'a', 'a', 'a']
+
+list1 = list('abcabcabc')
+list2 = list1.copy() # 깊은 복사
+list3 = list(list1) # 깊은 복사
+list4 = list1[::] # 깊은 복사
+list1[0] = '>>'
+print(list1) # ['>>', 'b', 'c', 'a', 'b', 'c', 'a', 'b', 'c']
+print(list2) # ['a', 'b', 'c', 'a', 'b', 'c', 'a', 'b', 'c']
+print(list3) # ['a', 'b', 'c', 'a', 'b', 'c', 'a', 'b', 'c']
+print(list4) # ['a', 'b', 'c', 'a', 'b', 'c', 'a', 'b', 'c']
+
+# 유의
+a = [1, 2, 3]
+b = ['a', 'b', 'c']
+a[1:2] = b
+b[1] = 111
+print(a) # [1, 'a', 'b', 'c', 3]
+print(b) # ['a', 111, 'c']
+
+
+print((1, 2, 3, 'abcd')) # (1, 2, 3, 'abcd')
+tuple1 = tuple('h' * 3)
+tuple1 += (4, ) # 하나일 땐 뒤에 콤마를 붙여야 튜플로 인식한다.
+tuple1 += (5, 6)
+print(tuple1) # ('h', 'h', 'h', 4, 5, 6)
+
+tuple1 = (1, 2, 3, 4, 5, 6)
+_, _, _, a, b, c = tuple1
+c, b, a = a, b, c
+print(a, b, c) # 6 5 4
+
+
+print({1, 2, 3, 4, 5}) # {1, 2, 3, 4, 5}
+print(set('12345')) # {'3', '5', '4', '1', '2'} # 집합은 순서와 무관
+
+s = set({1, 2, 3})
+s.add(5)
+print(s) # {1, 2, 3, 5}
+
+print(set({'a' : 'b', 'c' : 'd'})) # {'a', 'c'} # 딕셔너리의 키만 사용
+
+menus = {
+    'menu1' : {'재료1', '재료3'},
+    'menu2' : {'재료2', '재료3', '재료4'},
+    'menu3' : {'재료1', '재료3', '재료5'},
+    'menu4' : {'재료4', '재료5', '재료6'}
+}
+for menu, ingredients in menus.items():
+    if not '재료1' in ingredients:
+        print(menu)
+# menu2
+# menu4
+
+menus = {
+    'menu1' : {'재료1', '재료3'},
+    'menu2' : {'재료2', '재료3', '재료4'},
+    'menu3' : {'재료1', '재료3', '재료5'},
+    'menu4' : {'재료4', '재료5', '재료6'}
+}
+print(menus['menu1'] & menus['menu2']) # {'재료3'} # 교집합
+print(menus['menu1'] | menus['menu2']) # {'재료1', '재료3', '재료2', '재료4'} # 합집합
+print(menus['menu1'] - menus['menu2']) # {'재료1'} # 차집합
+print(menus['menu1'] ^ menus['menu2']) # {'재료1', '재료2', '재료4'} # 대칭차집합
+
+# 부분집합 판정
+print({1, 2, 3} < {1, 2, 3}) # False # 진부분집합
+print({1, 2, 3} <= {1, 2, 3}) # True # >, >=도 가능
