c09c1f1f89ac117d7668077cfe02b3408d57d939
dev_donggi <hi.donggi@gmail.com>
2019-10-09 11:35:25 +0900

    .

---
+++ Repositories/STS/config-server/src/main/java/io/github/donggi/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.donggi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
