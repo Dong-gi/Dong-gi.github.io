baa52199496029cca1f763970aade2da81483c23
Donggi Kim <dgkim@sesisoft.com>
2019-12-04 18:12:56 +0900

    .

---
+++ Repositories/STS/logback-maven/src/main/java/io/github/donggi/LogbackMavenApplication.java
@@ -0,0 +1,21 @@
+package io.github.donggi;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootApplication
+public class LogbackMavenApplication {
+    private static final Logger ERROR_LOG = LoggerFactory.getLogger("ERROR_LOG");
+    
+	public static void main(String[] args) {
+		SpringApplication.run(LogbackMavenApplication.class, args);
+		log.debug("Application really well started");
+		ERROR_LOG.error("에러가 발생한 거시애오");
+	}
+
+}
