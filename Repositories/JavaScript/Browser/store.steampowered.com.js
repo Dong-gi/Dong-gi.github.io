17f6b7c88056ca09dd5a8514aecda3b462cf1f23
dev_donggi <hi.donggi@gmail.com>
2020-03-29 22:39:49 +0900

    .

--- Repositories/JavaScript/Browser/store.steampowered.com.js
+++ Repositories/JavaScript/Browser/store.steampowered.com.js
@@ -1,13 +1,12 @@
 // Steam 목록 자동 "제외하기"
 // https://store.steampowered.com/search/?sort_by=Released_DESC
-class SteamGameRow {
-    constructor(title, release, discount, review, oriPrice, disPrice, optionNode) {
-        [this.title, this.release, this.discount, this.review, this.oriPrice, this.disPrice, this.optionNode] = arguments;
-    }
+function SteamGameRow(title, release, discount, review, oriPrice, disPrice, optionNode) {
+    [this.title, this.release, this.discount, this.review, this.oriPrice, this.disPrice, this.optionNode] = arguments;
 }
 var games = [];
 for (let a of document.querySelectorAll('a.search_result_row')) {
-    if (a.classList.contains('ds_ignored')) {
+    if (a.classList.contains('ds_owned') || a.classList.contains('ds_ignored') || a.classList.contains('ds_excluded_by_preferences') || a.classList.contains('ds_wishlist')) {
+        a.remove();
         continue;
     }
     try {
@@ -36,7 +35,8 @@ for (let a of document.querySelectorAll('a.search_result_row')) {
         }
         let price = a.querySelector('div.search_price').innerText.replace('무료 체험판', '369369').replace('무료', '0').replace(/[^\d\n]/gm, '').trim().split('\n');
         let game = new SteamGameRow(title, release, discount, review, price[0], price.length > 1? price[1] : price[0], a.querySelector('div.ds_options'));
-        //console.log(game);
+        if (!game.optionNode)
+            continue;   // 옵션 노드가 없으면 제거 불가능
         games.push(game);
     } catch (e) {
         console.log(e);
@@ -47,19 +47,27 @@ function sleep(ms) {
 }
 async function processGame() {
     if (games.length < 1) {
+        console.log('처리 완료');
         return;
     }
+
     let game = games[0];
     games = games.slice(1);
-    // if (game.review == 'positive') { // 긍정적이지 않은 것 모두 제거
-    if (game.disPrice == 0) { // 유료 모두 제거
-        processGame();
-        return;
-    }
     
-    if (!game.optionNode)
-    let position = game.optionNode.parentElement.offsetTop - 50;
-    if (position < 0) { // 이미 처리됨
+    let removeAnyway          = false;       // 무조건 삭제
+    let isPass = !removeAnyway;
+    if (!removeAnyway) {
+        let removeNotPositive = true;       // 긍정적이지 않은 것 제거
+        if (isPass && removeNotPositive)
+            isPass = removeNotPositive && !!game.review && game.review == 'positive';
+        let removeNotFree     = false;       // 유료 제거
+        if (isPass && removeNotFree)
+            isPass = removeNotFree && !!game.disPrice && game.disPrice == 0;
+    }
+    let position = game.optionNode.parentElement.offsetTop - 50; // 해당 게임 위치
+    isPass = isPass || (position < 0);      // 이미 처리됨
+    
+    if (isPass) {
         processGame();
         return;
     }
@@ -83,7 +91,7 @@ async function processGame() {
     options.dispatchEvent(new MouseEvent('mouseout', {view: window, bubbles: false, cancelable: true}));
     game.optionNode.parentElement.dispatchEvent(new MouseEvent('mouseout', {view: window, bubbles: false, cancelable: true}));
     options.remove();
-            
+    game.optionNode.parentElement.remove();    
     processGame();
 }
 processGame();
