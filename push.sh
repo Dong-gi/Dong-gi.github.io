14261407e505cb7ef1256d2ca700b40a0efa8409
Donggi Kim <hi.donggi@gmail.com>
2020-05-06 13:11:46 +0000

    .

--- push.sh
+++ push.sh
@@ -2,14 +2,15 @@ cd /workspace/donggi-github/Dong-gi.github.io
 
 /bin/bash build_site.sh
 
-for entry in $(find /workspace/donggi-github/Dong-gi.github.io/posts/ -name '*.html')
-do
-    html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true --output $entry $entry
-done
-echo 'post 압축 완료'
-
-html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true --output /workspace/donggi-github/Dong-gi.github.io/index.html /workspace/donggi-github/Dong-gi.github.io/index.html
-echo 'index 압축 완료'
+# 압축 시 +asCode() 깨짐
+#for entry in $(find /workspace/donggi-github/Dong-gi.github.io/posts/ -name '*.html')
+#do
+#    html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true --output $entry $entry
+#done
+#echo 'post 압축 완료'
+#
+#html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true --output /workspace/donggi-github/Dong-gi.github.io/index.html /workspace/donggi-github/Dong-gi.github.io/index.html
+#echo 'index 압축 완료'
 
 git add --all
 git commit -m '.'
