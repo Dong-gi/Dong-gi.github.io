550e772943546189b9111bf9635d1dc51a05fe1b
Donggi Kim <hi.donggi@gmail.com>
2023-08-29 08:44:12 +0000

    .

--- source/filelist.js
+++ source/filelist.js
@@ -3,6 +3,7 @@ M	pugs/infra/aws.pug
 M	pugs/infra/aws.pug
 M	pugs/infra/aws.pug
 M	pugs/infra/aws.pug
+M	pugs/infra/aws.pug
 M	pugs/algorithm/overview.pug
 A	pugs/book/071.pug
 A	pugs/book/072.pug
@@ -59,6 +60,4 @@ M	pugs/book/021.pug
 M	pugs/book/047.pug
 A	pugs/book/048.pug
 A	pugs/book/049.pug
-A	pugs/book/047.pug
-M	pugs/infra/OpenAPI.pug
 `;
