4cb3b657d2e2f69377ae7dd61afc0d680465d97a
Oracle Public Cloud User <opc@instance-20240718-1355.subnet02162141.vcn02162141.oraclevcn.com>
2024-12-05 13:02:37 +0000

    .

--- Repositories/JavaScript/Browser/draw2.js
+++ Repositories/JavaScript/Browser/draw2.js
@@ -8,6 +8,6 @@
 	let c = canvas.getContext('2d');
 	c.clearRect(0, 0, canvas.width, canvas.height);
 	let image = new Image();
-	image.src = "/imgs/kanna.gif";
+	image.src = "/imgs/no-date/kanna.gif";
 	image.onload = () => c.drawImage(image, 0, 0);
 })();
