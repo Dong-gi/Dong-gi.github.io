ebbadf1e20c4104e5f7ed16f1612676f5dc4b211
Oracle Public Cloud User <opc@instance-20240718-1355.subnet02162141.vcn02162141.oraclevcn.com>
2025-09-22 05:02:23 +0000

    .

---
+++ Repositories/Single/202509/22-1353.js
@@ -0,0 +1,27 @@
+/**
+ * @typedef SomeType
+ * Do something...
+ */
+
+/**
+ * # 섹션 제목
+ *
+ * - 글머리 기호1
+ * * 글머리 기호2
+ *
+ * **강조**
+ *
+ * _이탤릭체_
+ *
+ * > 인용 문구
+ *
+ * [링크](https://example.com)
+ *
+ * ```
+ * // 코드 블록
+ * let x = 1
+ * console.log(x)
+ * ```
+ * 
+ * {@link SomeType} 참고
+ */
